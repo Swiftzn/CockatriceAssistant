7b67473de3a270c3ec27d4b4f76cea0651356fb1
Swiftzn <dallenzn@gmail.com>
2025-11-07 10:55:23 +0000

    Update Version

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 """Version management for Cockatrice Assistant."""
 
 # Application version - update this when releasing new versions
-__version__ = "1.0.1"
-__version_info__ = (1, 0, 1)
+__version__ = "1.0.2"
+__version_info__ = (1, 0, 2)
 
 # GitHub repository information for update checking
 GITHUB_REPO_OWNER = "Swiftzn"
